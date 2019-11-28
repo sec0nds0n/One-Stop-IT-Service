474477ac2836a1eb72bb46024d7a5288e122e66c
omicon <muhammaddwison@gmail.com>
2019-11-28 13:06:14 +0700

    ayo

--- application/config/logout.php
+++
@@ -1,6 +0,0 @@
-<?php 
-    session_start();
-    session_destroy();
-    unset($_SESSION['nip']);
-    echo "<script>alert('Anda sudah logout!');window.location.href = '../prakomxiv';</script>";
-?>
--- application/views/news/success.php
+++
@@ -1,4 +0,0 @@
-<script>
-    alert('BERHASIL WEY');
-    window.location = 'http://localhost/CodeIgniter/news/';
-</script>
